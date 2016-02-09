0628732aff66e1f984639bd08602a36c332e67c2
groozin <tomasz.mikos@gmail.com>
2016-02-09 19:12:39 +0100

    Team Pulse creation.

--- TeamPulse/TeamPulseTests/TeamPulseTests.cs
+++ TeamPulse/TeamPulseTests/TeamPulseTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using Xunit;
 
 namespace TeamPulseTests
@@ -10,9 +11,49 @@ namespace TeamPulseTests
     public class TeamPulseTests
     {
         [Fact]
-        public void Monkey()
+        public void CurrentTeamMemberGetsProcessed()
         {
+            var repo = new Mock<ITeamMemberRepository>();
+            var processor = new Mock<ITeamPulseProcessor>();
+            var teamMember = new TeamMember();
+
+            TeamPulseCreator system = new TeamPulseCreator(repo.Object, processor.Object);
+            system.Process(2016, 4);
+
 
         }
+    }
+
+    public class TeamPulseCreator
+    {
+        public TeamPulseCreator(ITeamMemberRepository repo, ITeamPulseProcessor processor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TeamPulse Process(int year, int week)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TeamPulse
+    {
+        
+    }
+
+    public interface ITeamMemberRepository
+    {
+
+    }
+
+    public interface ITeamPulseProcessor
+    {
+
+    }
+
+    public class TeamMember
+    {
+
     }
 }
